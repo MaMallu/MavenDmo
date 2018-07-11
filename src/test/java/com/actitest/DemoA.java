b62d2fe0e9ad84777bb29e6d492e8925ab0bc852
mallumani <mallumani@mallumani-PC>
2018-07-11 10:25:30 +0530

    second commit

---
+++ src/test/java/com/actitest/DemoA.java
@@ -0,0 +1,13 @@
+package com.actitest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoA 
+{
+@Test
+public void testMethod()
+{
+	Reporter.log("hi............",true);
+}
+}
